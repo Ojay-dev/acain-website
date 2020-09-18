47cf0ca1014f2c49b1ba44a7211d8020b4f81519
Alikali Ojonugwa <alikali.ojonugwajustice@gmail.com>
2020-09-18 15:34:50 +0100

    finish header

--- src/components/header.js
+++ src/components/header.js
@@ -14,31 +14,33 @@ const nav = {
 }
 
 const HeaderLink = props => (
-  <li>
-    <Link to={props.to}>{props.text}</Link>
+  <li className={styles.navmenu__item}>
+    <Link to={props.to} className={styles.navmenu__link}>{props.text}</Link>
   </li>
 )
 
 export default () => {
   return (
-    <header>
+    <header className={styles.header}>
       <div className={styles.logo}>
         <img src={logo} alt="logo" />
       </div>
 
-      <nav>
-        <ul>
+      <nav className={styles.navmenu}>
+        <ul className={styles.navmenu__navlink}>
           {Object.values(nav).map((menu, idx) => (
             <HeaderLink to={menu.to} text={menu.text} key={idx} />
           ))}
         </ul>
+      </nav>
 
-        <ul>
-          <li>
-            <Link to="/">Log in</Link>
+      <nav>
+      <ul className={styles.navmenu__authlink}>
+          <li className={styles.navmenu__item}>
+            <Link to="/" className={styles.navmenu__link}>Log in</Link>
           </li>
-          <li>
-            <Link to="/">Join</Link>
+          <li className={styles.navmenu__item}>
+            <Link to="/" className={`${styles.navmenu__link} ${styles.navmenu__join}`}>Join</Link>
           </li>
         </ul>
       </nav>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import styles from "./index.module.scss"
+// import styles from "./index.module.scss"
 import Layout from "../components/layout"
 
 export default function Home() {
