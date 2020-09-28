9d589e397341db7c5ec1098e13ef42dcb0d21cf1
Alikali Ojonugwa <alikali.ojonugwajustice@gmail.com>
2020-09-28 01:16:52 +0100

    Modify Signin page to use reusable component

---
+++ src/components/input.js
@@ -0,0 +1,40 @@
+import React from "react"
+import styles from "./input.module.scss"
+import logo from "../svgs/logo-header.svg"
+
+export function Input(props) {
+  return (
+    <>
+      <label htmlFor={`${props.label.toLowerCase()}`} className={styles.label}>
+        {props.label}
+      </label>
+      <input
+        type={`${props.type}`}
+        name={`${props.label.toLowerCase()}`}
+        placeholder={props.placeholder}
+        className={styles.input}
+      />
+    </>
+  )
+}
+
+export function SubmitInput(props) {
+  return (
+    <div className={styles.submit}>
+      <input
+        type="submit"
+        value={props.value}
+        className={styles.submit__input}
+      />
+    </div>
+  )
+}
+
+export function FormTitle(props) {
+  return (
+    <div>
+      <img src={logo} alt="logo" />
+      <h2 className={styles.title}>{props.title}</h2>
+    </div>
+  )
+}
--- src/pages/sign-in.js
+++ src/pages/sign-in.js
@@ -1,38 +1,31 @@
 import React from "react"
+import { Link } from "gatsby"
 import styles from "./sign-in.module.scss"
-import logo from "../svgs/logo-header.svg"
+
+import { FormTitle, Input, SubmitInput as Submit } from "../components/input"
 
 export default function () {
   return (
     <div className={styles.login}>
-      <div>
-        <img src={logo} alt="logo" />
-        <h2 className={styles.login__title}>Sign In</h2>
-      </div>
+      <FormTitle title="Sign In" />
 
       <form className={styles.login__form}>
         <div className={styles.login__row}>
-          <label htmlFor="email" className={styles.login__label}>
-            Email
-          </label>
-          <input type="text" name="email" placeholder="abc@xyz.com" className={styles.login__input} />
+          <Input label="Email" placeholder="abc@xyz.com" type="text"/>
         </div>
 
         <div className={styles.login__row}>
-          <label htmlFor="password" className={styles.login__label}>
-            Password
-          </label>
-          <input type="password" name="password" placeholder="Password" className={styles.login__input} />
+          <Input label="Password" placeholder="Password"  type="password"/>
         </div>
 
         <span className={styles.login__subtext}>
           Not yet a member?{" "}
-          <a href="#" className={styles.login__link}>
+          <Link to="/join" className={styles.login__link}>
             Join
-          </a>
+          </Link>
         </span>
 
-        <div className={styles.login__submitDiv}><input type="submit" value="Sign in" className={styles.login__submit} /></div>
+        <Submit value="Sign in" />
       </form>
     </div>
   )
