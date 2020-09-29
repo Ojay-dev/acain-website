dc7517d506773971b02f327fb8975b40a2e8eb7a
Alikali Ojonugwa <alikali.ojonugwajustice@gmail.com>
2020-09-29 10:33:37 +0100

    Add page subtitle

--- src/components/page-title.js
+++ src/components/page-title.js
@@ -5,6 +5,12 @@ export default ({ children }) => {
   return (
     <div className={styles.pageTitle}>
       <h2 className={styles.pageTitle__text}>{children}</h2>
+      <div className={styles.pageTitle__subtext}>
+        Home/
+        <span className={styles.pageTitle__span}>
+          {children.split(" ").join("-")}
+        </span>
+      </div>
     </div>
   )
 }
