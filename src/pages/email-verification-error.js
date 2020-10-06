3fbe93d14491ef78cb15346b52a8c829abedac0a
Alikali Ojonugwa <alikali.ojonugwajustice@gmail.com>
2020-10-06 01:28:23 +0100

    Add email-verification-error page

---
+++ src/pages/email-verification-error.js
@@ -0,0 +1,28 @@
+import React from "react"
+import Layout from "../components/layout"
+import styles from "./email-verification-error.module.scss"
+
+import email_error from "../svgs/email-error.svg"
+
+export default function () {
+  return (
+    <Layout>
+      <div className={styles.verifyError}>
+        <h3 className={styles.verifyError__title}>
+          Oops! looks like something went wrong!!
+        </h3>
+        <img
+          src={email_error}
+          alt="error"
+          className={styles.verifyError__image}
+        />
+        <span className={styles.verifyError__text}>
+          Click on this link to retry your Email verification
+        </span>
+        <a href="#" className={styles.verifyError__link}>
+          Retry
+        </a>
+      </div>
+    </Layout>
+  )
+}
