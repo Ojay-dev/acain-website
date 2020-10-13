5924b2438f03cef0f0fb67751bbacf365139c166
Alikali Ojonugwa <alikali.ojonugwajustice@gmail.com>
2020-10-13 17:26:57 +0100

    Show Loader component before making payment

--- src/pages/welcome.js
+++ src/pages/welcome.js
@@ -1,10 +1,47 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import axios from "axios"
 import { Link, navigate } from "gatsby"
-import { logout } from "../services/auth"
+import Loader from "../components/loader"
+import { logout, getUser } from "../services/auth"
 import styles from "./welcome.module.scss"
 import payment from "../svgs/payment.svg"
 
 export default function () {
+  const [loading, setLoading] = useState(false)
+  useEffect(() => {
+    console.log(getUser())
+  })
+
+  const proceedWithPayment = async () => {
+    // console.log(getUser().membershipType)
+    const config = {
+      headers: { Authorization: `Bearer ${getUser().access_token}` },
+    }
+
+    const bodyParameters = {
+      membershipType: getUser().membershipType,
+    }
+
+    try {
+      setLoading(true)
+      const resp = await axios.post(
+        "https://acain.herokuapp.com/api/v1/pay",
+        bodyParameters,
+        config
+      )
+      const { data } = resp.data
+      // console.log(data)
+      window.location = data.link
+    } catch (error) {
+      setLoading(false)
+      console.log(error)
+    }
+  }
+
+  if (loading) {
+    return <Loader />
+  }
+
   return (
     <div className={styles.welcome}>
       <h2 className={styles.welcome__text}>Welcome, Alfred Chado.</h2>
@@ -15,9 +52,12 @@ export default function () {
 
       <img src={payment} alt="pasture icon" className={styles.welcome__image} />
 
-      <Link to="#" className={styles.welcome__paymentLink}>
+      <button
+        className={styles.welcome__paymentLink}
+        onClick={proceedWithPayment}
+      >
         Make payment
-      </Link>
+      </button>
       <Link
         to="#"
         className={styles.welcome__backLink}
