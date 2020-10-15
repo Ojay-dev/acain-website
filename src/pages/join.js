6f461661150db44fd47a534dde8f21fac4cf49fe
Alikali Ojonugwa <alikali.ojonugwajustice@gmail.com>
2020-10-15 19:32:08 +0100

    Optimise join error message using snackbar component

--- src/pages/join.js
+++ src/pages/join.js
@@ -4,13 +4,18 @@ import { Link } from "gatsby"
 import { useForm } from "react-hook-form"
 import Loader from "../components/loader"
 import { FormTitle } from "../components/input"
+import SnackBar from "../components/snackBar"
 import { StepOne, StepTwo, StepThree } from "../components/joinFormSteps"
 import styles from "./join.module.scss"
 import { handleJoin } from "../services/auth"
 
 export default function Join() {
   const [currentStep, setCurrentStep] = useState(1)
-  const [serverSideError, setServerSideError] = useState()
+  const [openSnackbar, setOpenSnackbar] = useState({
+    open: false,
+    status: null,
+    message: "",
+  })
   const [loading, setLoading] = useState(false)
   const {
     register,
@@ -74,6 +79,14 @@ export default function Join() {
     return null
   }
 
+  const handleOpen = newState => {
+    setOpenSnackbar({ open: true, ...newState })
+  }
+
+  const handleClose = () => {
+    setOpenSnackbar({ ...openSnackbar, open: false })
+  }
+
   const onSubmit = async data => {
     if (data.email) {
       setCurrentStep(2)
@@ -100,64 +113,69 @@ export default function Join() {
         },
       }
 
-
       try {
         setLoading(true)
-        await setTimeout(async () => {
-          await handleJoin(userDetails)
-        }, 5000)
+        await handleJoin(userDetails)
         navigate(`/welcome`)
       } catch (e) {
         // console.log(e.response)
-        setServerSideError(e.response.data.message)
+        const { status, message } = e.response.data
         setLoading(false)
+        handleOpen({
+          status,
+          message,
+        })
       }
     }
   }
 
-  if (loading) {
-    return <Loader />
-  }
+  // if (loading) {
+  //   return <Loader />
+  // }
 
   return (
-    <div className={styles.join}>
-      <FormTitle title="Become a member" />
+    <React.Fragment>
+      <div className={styles.join}>
+        <FormTitle title="Become a member" />
+        <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+          <StepOne
+            currentStep={currentStep}
+            register={register}
+            watch={watch}
+            errors={errors}
+          />
+          <StepTwo
+            currentStep={currentStep}
+            register={register}
+            errors={errors}
+          />
+          <StepThree
+            currentStep={currentStep}
+            register={register}
+            watch={watch}
+            errors={errors}
+            getValues={getValues}
+            trigger={trigger}
+          />
 
-      {serverSideError ? (
-        <span style={{ color: "red" }}>{serverSideError}</span>
-      ) : null}
-
-      <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
-        <StepOne
-          currentStep={currentStep}
-          register={register}
-          watch={watch}
-          errors={errors}
-        />
-        <StepTwo
-          currentStep={currentStep}
-          register={register}
-          errors={errors}
-        />
-        <StepThree
-          currentStep={currentStep}
-          register={register}
-          watch={watch}
-          errors={errors}
-          getValues={getValues}
-          trigger={trigger}
-        />
-
-        <div className={styles.join__buttonArea}>
-          <Link to="/sign-in" className={styles.join__link}>
-            Sign in instead
-          </Link>
-          <div>
-            {previousButton()}
-            {nextButton()}
+          <div className={styles.join__buttonArea}>
+            <Link to="/sign-in" className={styles.join__link}>
+              Sign in instead
+            </Link>
+            <div>
+              {previousButton()}
+              {nextButton()}
+            </div>
           </div>
-        </div>
-      </form>
-    </div>
+        </form>
+      </div>
+
+      <SnackBar
+        openSnackbar={openSnackbar}
+        handleClose={handleClose}
+        severity={openSnackbar.status === 201 ? "success" : "error"}
+        message={openSnackbar.message}
+      />
+    </React.Fragment>
   )
 }
