15a9f5f0b5f170bc2d292c6b1d2400c3910e0143
Alikali Ojonugwa <alikali.ojonugwajustice@gmail.com>
2020-10-21 14:32:15 +0100

    Add coming soon directories

---
+++ src/pages/members-directory.js
@@ -0,0 +1,101 @@
+import React from "react"
+
+import ComingSoon from "../components/coming-soon"
+// import Layout from "../components/layout"
+// import Title from "../components/page-title"
+// import styles from "./members-directory.module.scss"
+// import image from "../../content/board-images/tonye-faloughi-ekezie.png"
+// import facebook from "../svgs/facebook-gray.svg"
+// import instagram from "../svgs/instagram-gray.svg"
+// import linkedin from "../svgs/linkedin-gray.svg"
+// import twitter from "../svgs/twitter-gray.svg"
+
+export default function () {
+  return <ComingSoon />
+  // return (
+  //   <Layout>
+  //     <Title>Members directory</Title>
+  //     <div className={styles.memberDirectory}>
+  //       <div className={styles.memberDirectory__card}>
+  //         <img src={image} alt="tonye" />
+  //         <div className={styles.cardDetail}>
+  //           <h4 className={styles.cardDetail__name}>Tonye Faloughi-Ekezie</h4>
+  //           <span className={styles.cardDetail__proffession}>
+  //             Author, Illustartor
+  //           </span>
+  //           <span className={styles.cardDetail__location}>Ibadan, Nigeria</span>
+  //           <h5 className={styles.cardDetail__subtitle}>Contact Details</h5>
+  //           <span className={styles.cardDetail__email}>abdxchr@gmail.com</span>
+  //           <span className={styles.cardDetail__number}>0701 2345 897</span>
+  //           <div className={styles.cardDetail__social}>
+  //             {/* <a href="#">
+  //               <img src={facebook} alt="facecbook icon" />
+  //             </a>
+  //             <a href="#">
+  //               <img src={twitter} alt="twitter icon" />
+  //             </a>
+  //             <a href="#">
+  //               <img src={instagram} alt="instagram icon" />
+  //             </a>
+  //             <a href="#">
+  //               <img src={linkedin} alt="linkedin icon" />
+  //             </a> */}
+  //           </div>
+  //         </div>
+  //       </div>
+
+  //       <div className={styles.memberDirectory__card}>
+  //         <img src={image} alt="tonye" />
+  //         <div className={styles.cardDetail}>
+  //           <h4>Tonye Faloughi-Ekezie</h4>
+  //           <span>Author, Illustartor</span>
+  //           <span>Ibadan, Nigeria</span>
+  //           <h5>Contact Details</h5>
+  //           <span>abdxchr@gmail.com</span>
+  //           <span>0701 2345 897</span>
+  //           <div className={styles.cardDetail__social}>
+  //             {/* <a href="#">
+  //               <img src={facebook} alt="facecbook icon" />
+  //             </a>
+  //             <a href="#">
+  //               <img src={twitter} alt="twitter icon" />
+  //             </a>
+  //             <a href="#">
+  //               <img src={instagram} alt="instagram icon" />
+  //             </a>
+  //             <a href="#">
+  //               <img src={linkedin} alt="linkedin icon" />
+  //             </a> */}
+  //           </div>
+  //         </div>
+  //       </div>
+
+  //       <div className={styles.memberDirectory__card}>
+  //         <img src={image} alt="tonye" />
+  //         <div className={styles.cardDetail}>
+  //           <h4>Tonye Faloughi-Ekezie</h4>
+  //           <span>Author, Illustartor</span>
+  //           <span>Ibadan, Nigeria</span>
+  //           <h5>Contact Details</h5>
+  //           <span>abdxchr@gmail.com</span>
+  //           <span>0701 2345 897</span>
+  //           <div className={styles.cardDetail__social}>
+  //             {/* <a href="#">
+  //               <img src={facebook} alt="facecbook icon" />
+  //             </a>
+  //             <a href="#">
+  //               <img src={twitter} alt="twitter icon" />
+  //             </a>
+  //             <a href="#">
+  //               <img src={instagram} alt="instagram icon" />
+  //             </a>
+  //             <a href="#">
+  //               <img src={linkedin} alt="linkedin icon" />
+  //             </a> */}
+  //           </div>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   </Layout>
+  // )
+}
